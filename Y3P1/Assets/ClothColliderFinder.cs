7c548968cedb5b6e6453b25fb6e3211807497e2e
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-10-31 14:19:33 +0100

    Cowl

---
+++ Y3P1/Assets/ClothColliderFinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothColliderFinder : MonoBehaviour {
+
+    public CapsuleCollider col1;
+    public CapsuleCollider col2;
+    public Cloth myCloth;
+
+    private void Start() 
+    {
+        myCloth = GetComponent<Cloth>();
+
+        col1 = transform.root.Find("CowlCollider1").GetComponent<CapsuleCollider>();
+        col2 = transform.root.Find("CowlCollider2").GetComponent<CapsuleCollider>();
+
+        myCloth.capsuleColliders[0] = col1;
+        myCloth.capsuleColliders[1] = col2;
+    }
+}
