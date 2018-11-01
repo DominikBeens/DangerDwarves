837e7fa4a5a94c23b42ebf3b036cea213ca5489a
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-11-01 12:16:42 +0100

    Vol lighting, removed DoF

---
+++ Y3P1/Assets/Scripts/Wouter/ForceThisFuckerOn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AuraAPI;
+
+public class ForceThisFuckerOn : MonoBehaviour {
+
+    private AuraLight myAuraLight;
+
+    private void Start()
+    {
+        myAuraLight = GetComponent<AuraLight>();
+    }
+
+    private void Update()
+    {
+        if(!myAuraLight.isActiveAndEnabled)
+        {
+            myAuraLight.enabled = true;
+        }
+    }
+}
