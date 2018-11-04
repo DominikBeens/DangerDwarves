0547e1275cb188d9b038dc6e7529045bdb54fa68
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-11-04 16:23:33 +0100

    Implemented melee attack sounds

---
+++ Y3P1/Assets/Scripts/Wouter/PickAndPlayFromList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickAndPlayFromList : MonoBehaviour {
+
+    //public string listName;
+
+    public List<AudioSource> SFXSwingSounds = new List<AudioSource>();
+
+
+    public void PlaySFXSwingSoundFromList()
+    {
+        SFXSwingSounds[Random.Range(0, SFXSwingSounds.Count)].Play();
+    }
+}
