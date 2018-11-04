befda5ed0c010763e25aa073df2a0c2c42aeb9c7
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-11-04 21:45:51 +0100

    Implemented Barhub Attic

---
+++ Y3P1/Assets/OnOffObj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffObj : MonoBehaviour {
+
+    public GameObject obj;
+
+    public void TurnOnObj()
+    {
+        obj.SetActive(true);
+    }
+
+    public void TurnOffObj()
+    {
+        obj.SetActive(false);
+    }
+}
+
