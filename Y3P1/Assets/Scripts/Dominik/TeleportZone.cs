ed357104aabce8c1dfc2c992891bc052fae7140b
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-04 20:34:45 +0100

    ghost particle tweak, network id stuff, added teleport zone

---
+++ Y3P1/Assets/Scripts/Dominik/TeleportZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Y3P1;
+
+public class TeleportZone : MonoBehaviour
+{
+
+    public void Teleport(Transform destination)
+    {
+        Player.localPlayer.teleporter.Teleport(destination.position);
+    }
+}
