ae46008700da51a5426786b8044b1523c7e2c435
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-11-05 14:19:02 +0100

    fog turned off, yay frames

--- Y3P1/Assets/LocalAmbienceController.cs
+++ Y3P1/Assets/LocalAmbienceController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Y3P1;
 
 public class LocalAmbienceController : MonoBehaviour {
 
+    public GameObject playercamera;
+
+    public VolumetricFog myFogSettings;
+
+    public float fogNoiseAmount;
+    public float fogFoggyness;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        playercamera = Player.localPlayer.playerCam.gameObject;
+        myFogSettings = playercamera.GetComponent<VolumetricFog>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
