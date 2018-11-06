f4148e18a9a8501eef1d82e7ccde393a8daeb0f0
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-11-06 21:08:20 +0100

    Added sounds to crates, most spells

---
+++ Y3P1/Assets/Wouter/PickAndPlayOnStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickAndPlayOnStart : MonoBehaviour {
+
+    public List<AudioSource> SFXList = new List<AudioSource>();
+
+    private void Start()
+    {
+        SFXList[Random.Range(0, SFXList.Count)].Play();
+    }
+}
