a51dd6597ba95b388b9cd1a58c54f63ae64b59d9
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-06 21:41:28 +0100

    the hub and lit braziers heal the player over time

---
+++ Y3P1/Assets/Scripts/Dominik/PassiveHealZone.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PassiveHealZone : MonoBehaviour
+{
+
+    private float nextTick;
+    private Collider[] entitiesInRange = new Collider[10];
+
+    [SerializeField] private int healAmount = 1;
+    [SerializeField] private float healInterval = 1f;
+    [SerializeField] private float healRange;
+    [SerializeField] private LayerMask hitLayerMask;
+
+    private void Update()
+    {
+        if (Time.time >= nextTick)
+        {
+            nextTick = Time.time + healInterval;
+            Heal();
+        }
+    }
+
+    private void Heal()
+    {
+        int collidersFound = Physics.OverlapSphereNonAlloc(transform.position, healRange, entitiesInRange, hitLayerMask);
+
+        for (int i = 0; i < collidersFound; i++)
+        {
+            if (entitiesInRange[i].tag == "Player" && entitiesInRange[i].gameObject.layer == 9)
+            {
+                Entity entity = entitiesInRange[i].GetComponent<Entity>();
+                entity.Hit(healAmount, Stats.DamageType.AOE);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, healRange);
+    }
+}
