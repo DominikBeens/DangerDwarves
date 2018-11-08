36d751bf0793afb89c714b92658acf35f76fd914
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-08 13:08:25 +0100

    wraith has his summon demondwarf attack now

---
+++ Y3P1/Assets/Scripts/Dominik/Components/SpawnNetworkedObjOnFire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class SpawnNetworkedObjOnFire : MonoBehaviour 
+{
+
+    [SerializeField] private GameObject toSpawn;
+
+    private void Awake()
+    {
+        Projectile parentProjectile = GetComponentInParent<Projectile>();
+        if (parentProjectile)
+        {
+            parentProjectile.OnFire += (t) =>
+            {
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    PhotonNetwork.InstantiateSceneObject(toSpawn.name, transform.position, Quaternion.identity);
+                }
+            };
+        }
+    }
+}
