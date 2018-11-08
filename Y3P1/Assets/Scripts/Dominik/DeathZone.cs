3347dc6be2438e1d857fae3f296f8cf173079241
Lajos de Graaf <lajosdegraaf@hotmail.com>
2018-11-08 17:40:52 +0100

    Ladder and colliders

---
+++ Y3P1/Assets/Scripts/Dominik/DeathZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DeathZone : MonoBehaviour 
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && other.gameObject.layer == 9)
+        {
+            Entity entity = other.GetComponent<Entity>();
+            if (entity)
+            {
+                entity.Hit(-10000, Stats.DamageType.Melee);
+            }
+        }
+    }
+}
