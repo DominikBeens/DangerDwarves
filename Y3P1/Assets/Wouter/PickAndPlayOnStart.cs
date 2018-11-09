9c4c58e77584d72ee75191687ad423726b217d6e
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-11-09 11:53:46 +0100

    Ghost Sounds Inplemented

--- Y3P1/Assets/Wouter/PickAndPlayOnStart.cs
+++ Y3P1/Assets/Wouter/PickAndPlayOnStart.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PickAndPlayOnStart : MonoBehaviour {
 
     public List<AudioSource> SFXList = new List<AudioSource>();
+    public bool useFunctionInstead;
 
     private void Start()
     {
+        if(!useFunctionInstead)
+        {
+            SFXList[Random.Range(0, SFXList.Count)].Play();
+        }
+        
+    }
+
+    public void Function()
+    {
+
         SFXList[Random.Range(0, SFXList.Count)].Play();
     }
 }
