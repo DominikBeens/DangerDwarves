67c92685046360dcbff3ac85a891b4f844838360
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-09 09:40:18 +0100

    networked event code, made orb icon smaller, hub collider fix

---
+++ Y3P1/Assets/Scripts/Dominik/Components/NetworkedEvent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkedEvent : MonoBehaviour 
+{
+
+    private List<GameObject> players = new List<GameObject>();
+
+    [SerializeField] private Animator anim;
+    [SerializeField] private string animBoolName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && other.GetComponent<Entity>())
+        {
+            if (!players.Contains(other.gameObject))
+            {
+                players.Add(other.gameObject);
+                CheckPlayerCount();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && other.GetComponent<Entity>())
+        {
+            if (players.Contains(other.gameObject))
+            {
+                players.Remove(other.gameObject);
+            }
+        }
+    }
+
+    private void CheckPlayerCount()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (players.Count == PhotonNetwork.CountOfPlayers)
+            {
+                TriggerEvent(true);
+            }
+        }
+    }
+
+    public void TriggerEvent(bool b)
+    {
+        anim.SetBool(animBoolName, b);
+    }
+}
